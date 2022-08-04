47a1a28460716201d1083aaa24c3b4f35f01541f
en-one <Ab960303>
2022-08-04 22:08:23 +0800

    feat: add 删除重复节点2

--- linkList/linklist.go
+++ linkList/linklist.go
@@ -54,22 +54,40 @@ func MergeTwoList(l1, l2 *ListNode) *ListNode {
 // DeleteDuplicatesSaveOne 删除重复节点, 使得每个节点只出现一次
 func DeleteDuplicatesSaveOne(head *ListNode) *ListNode {
 	current := head
-
 	for current != nil {
 		for current.Next != nil && current.Val == current.Next.Val {
 			current.Next = current.Next.Next
 		}
 		current = current.Next
-
 	}
 
 	return head
-
 }
 
-//func deleteDuplicates(head *ListNode) *ListNode {
-//
-//}
+// deleteDuplicates 删除所有重复节点
+func DeleteDuplicatesWithNoOne(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	dummy := &ListNode{Val: 0}
+	dummy.Next = head
+	head = dummy
+
+	var rmVal int
+	for head.Next != nil && head.Next.Next != nil {
+		if head.Next.Val == head.Next.Next.Val {
+			rmVal = head.Next.Val
+			for head.Next != nil && head.Next.Val == rmVal {
+				head.Next = head.Next.Next
+			}
+		} else {
+			head = head.Next
+		}
+	}
+
+	return dummy.Next
+}
 
 //----------------------------------环-----------------------------------------------
 
