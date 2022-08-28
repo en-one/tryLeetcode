a63b7bca988362f20b6530f74572a75e670f22fb
zhangliyu <zhangliyu@shining3d.com>
2022-08-28 17:05:53 +0800

    test: 双指针原地删除节点

---
+++ array/dulpointer_test.go
@@ -0,0 +1,24 @@
+package array
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{[]int{1, 1, 2}}, 2},
+		{"test2", args{[]int{1}}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
