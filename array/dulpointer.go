d38218f82b507cff8835ce912bd23c1700d86f7f
zhangliyu <zhangliyu@shining3d.com>
2022-08-28 17:37:28 +0800

    feat: 数组的左右指针使用

--- array/dulpointer.go
+++ array/dulpointer.go
@@ -1,5 +1,7 @@
 package array
 
+import "fmt"
+
 // 索引作为指针
 
 // -------------------- 快慢指针，原地修改数组----------------------------
@@ -20,6 +22,31 @@ func removeDuplicates(nums []int) int {
 	}
 
 	return slow + 1
+}
+
+// 2、移除指定元素，返回长度  nums = [0,1,2,2,3,0,4,2], val = 2
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
+
+// 3、所有 0 移动到数组的末尾 : nums = [0,1,0,3,12]  [1,3,12,0,0]
+func moveZeroes(nums []int) {
+	i := removeElement(nums, 0)
+	for ; i < len(nums); i++ {
+		nums[i] = 0
+	}
+	fmt.Println(nums)
 }
 
 // -------------------- 左右指针----------------------------
